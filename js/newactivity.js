0d9671fab367d8c8f3e7228aee0c9773aa64a3a3
janiceschnydrig15 <janice.schnydrig@gmail.com>
2023-11-06 11:34:33 +0100

    newactivity

--- js/newactivity.js
+++ js/newactivity.js
@@ -1,36 +1,45 @@
 import { supa } from "/js/supabase.js";
-async function createPost(event) {
-    event.preventDefault();
+
+
+document.getElementById('newactivity').addEventListener('click', async () => {
+    const adresse = document.getElementById('adresse').value;
     const placedescription = document.getElementById('placedescription').value;
     const kindofactivity = document.getElementById('kindofactivity').value;
     const needhelp = document.getElementById('needhelp').value;
     const kindofknowledge = document.getElementById('kindofknowledge').value;
-
-    const user = await supa.auth.user();
-
-
-    if (user) {
-        const { data, error } = await supa
-            .from('post')
-            .insert([
-                {
-                    user_id: user.id,
-                    placedescription: placedescription,
-                    kindofactivity: kindofactivity, 
-                    needhelp: needhelp,
-                    kindofknowledge: kindofknowledge,
-                }
-            ]);
-            if (error) {
-                console.log("Error during create new post: ", error.message);
-            } else {
-                console.log("Post wurde erfolgreich gespeichert:");
-                window.location.href = "bestaetigung.html";
-            }
-        }
-    }
+    const photoInput = document.getElementById('photo');
     
-    document.getElementById('postForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        createPost();
-    });
+
+        try {
+            // Daten in die Supabase-Datenbank einfügen
+            const { data, error } = await supa
+                .from('post')
+                .insert([
+                    { adresse, placedescription, kindofactivity, needhelp, kindofknowledge },
+                ]);
+
+            if (error) {
+                throw error;
+            }
+
+        // Foto hochladen
+        const { data: fileData, error: fileError } = await supa
+            .storage
+            .from('profilpic') // Ersetze 'dein_bucket_name' durch den Namen deines Buckets
+            .upload(`public/${photoFile.name}`, photoFile);
+
+        if (fileError) {
+            throw fileError;
+        }
+    
+
+        console.log('Daten erfolgreich in Supabase eingefügt:', data);
+        console.log('Foto erfolgreich hochgeladen:', fileData);
+    } catch (error) {
+        console.error('Fehler beim Einfügen der Daten:', error);
+
+    } else {
+        console.error('Es wurde kein Foto ausgewählt.');
+    }
+    }
+});
