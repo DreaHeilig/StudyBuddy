ef939b9901d23f38e68bceaf77b5ea65be05fdb5
DreaHeilig <drea.heilig@gmail.com>
2023-11-09 16:55:45 +0100

    yooo notifs

--- js/notifications.js
+++ js/notifications.js
@@ -31,27 +31,63 @@ async function getUserInfo() {
 getUserInfo();
 
 async function fetchRequests() {
-    const userEmail = await getUserInfo(); // Wait for userEmail
-  
+    const userEmail = await getUserInfo();
+
     if (userEmail) {
-      const formattedUserEmail = userEmail.trim().toLowerCase(); // Convert to lowercase and remove white spaces
-  
-      const { data, error } = await supa
-        .from('request') // Assuming 'public' is the schema name
-        .select('id, user_email, post_email')
-        .eq('post_email', formattedUserEmail); // Use the formatted email for comparison
-  
-      if (error) {
-        console.error("Error fetching requests: ", error.message);
-      } else {
-        if (data && data.length > 0) { // Check if there's any data
-          const requestIds = data.map((request) => request.id);
-          console.log("Request IDs: ", requestIds);
+        const formattedUserEmail = userEmail.trim().toLowerCase();
+
+        const { data, error } = await supa
+            .from('request')
+            .select('id, user_email')
+            .eq('post_email', formattedUserEmail);
+
+        if (error) {
+            console.error("Error fetching requests: ", error.message);
         } else {
-          console.log("No matching requests found.");
+            if (data && data.length > 0) {
+                data.forEach((request) => {
+                    const id = request.id;
+                    const user_email = request.user_email;
+                    appendNotification(id, user_email);
+                });
+            } else {
+                console.log("No matching requests found.");
+            }
         }
-      }
     }
-  }
+}
+
+function appendNotification(id, user_email) {
+    const notifContainerAll = document.getElementById("notifContainerAll");
+    const notifContainer = document.createElement("div");
+    notifContainer.className = "notifContainer";
+    notifContainer.id = "notif" + id;
+
+    const notifText = document.createElement("p");
+    notifText.className = "notifText";
+    notifText.textContent = user_email + " möchte dein StudyBuddy sein!";
+    
+    const acceptButton = document.createElement("button");
+    acceptButton.id = "acceptButton" + id;
+    const acceptButtonImg = document.createElement("img");
+    acceptButtonImg.className = "notifButton";
+    acceptButtonImg.src = "img/checkmark.svg";
+    acceptButtonImg.alt = "Anfrage Annehmen";
+    acceptButton.appendChild(acceptButtonImg);
+
+    const declineButton = document.createElement("button");
+    declineButton.id = "declineButton" + id;
+    const declineButtonImg = document.createElement("img");
+    declineButtonImg.className = "notifButton";
+    declineButtonImg.src = "img/Cross.svg";
+    declineButtonImg.alt = "Anfrage Ablehnen";
+    declineButton.appendChild(declineButtonImg);
+
+    notifContainer.appendChild(notifText);
+    notifContainer.appendChild(acceptButton);
+    notifContainer.appendChild(declineButton);
+
+    notifContainerAll.appendChild(notifContainer);
+}
 
 fetchRequests();
